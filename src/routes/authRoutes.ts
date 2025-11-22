91f046c0fad97a3ad0a67972c736ff5f6ade8664
Blake <mr.blake.hero@mail.ru>
2025-11-22 14:37:59 +0300

    add login

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 import { body, validationResult } from 'express-validator'
 import User from '../models/User'
 import { env } from '../config/env'
+import UserSchema from "../models/User";
 
 const router = Router();
 
@@ -40,6 +41,39 @@ router.post('/api/register',
             username,
             visualName,
         })
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            env.jwtSecret,
+            { expiresIn: "365d" }
+        )
+
+        res.json( { user, token } )
+    }
+)
+
+router.post('/api/login',
+    [
+        body('email').isEmail(),
+        body('password').exists()
+    ],
+    async (req :Request, res :Response) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        const user  = await User.findOne({ email })
+        if(!user) {
+            return res.status(404).json( { message: 'User not found' } );
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
         const token = jwt.sign(
             { id: user._id, role: user.role },
