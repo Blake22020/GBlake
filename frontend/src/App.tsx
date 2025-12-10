649f1077dd8adc067d3ddbbf984a153123953467
Blake <mr.blake.hero@mail.ru>
2025-12-10 22:31:21 +0300

    add mainNavbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MainHeader from './components/headers/mainHeader'
 import LoginHeader from "./components/headers/loginHeader";
+import MainNavbar from './components/navbars/mainNavbar'
 
 function App() {
   return (
     <div className="App">
         <LoginHeader />
+        <MainNavbar />
     </div>
   );
 }
