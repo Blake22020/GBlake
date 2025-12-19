8dd372918bc1bfc895c6e8f2e07ccc7cf8a22e01
Blake <mr.blake.hero@mail.ru>
2025-12-19 19:48:40 +0300

    fix snowfall

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,15 +5,11 @@ import MainNavbarHeader from "./layouts/mainNavbarHeader";
 import MainPage from './pages/main'
 import Register from './pages/register'
 import Register2 from './pages/register2';
-import Snowfall from 'react-snowfall'
 
 function App() {
   return (
     <div className="App">
-		<Snowfall
-			color="#fff"
-			style={{height: '100%'}}
-		/>   
+  
 		<MainPage />
     </div>
   );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css'
+import Snowfall from 'react-snowfall'
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+		<Snowfall
+			color="#fff"
+			style={{height: '100%'}}
+		/> 
+		<App />
   </React.StrictMode>
 );
