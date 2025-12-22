ca90ba25577ca6f8aa28a9fab1ed0565dca04dcb
Blake <mr.blake.hero@mail.ru>
2025-12-22 21:03:50 +0300

    fiix

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ app.use("/api/search", searchRoutes)
 const clientPath = path.join(ROOT, 'frontend', 'build');
 app.use(express.static(clientPath));
 
-app.get('/*fallback', (req, res) => {
+app.get('/:any(*)', (req, res) => {
   if (req.path.startsWith('/api')) {
     return res.sendStatus(404);
   }
