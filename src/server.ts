069c142876d2c197fe8f1cd96925d1640945ff95
Blake <mr.blake.hero@mail.ru>
2025-12-22 10:37:35 +0300

    add react build

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import path from 'path'
 
+const __dirname__ = process.cwd();
+
 import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
 import postRoutes from './routes/postRoutes';
@@ -16,11 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-app.get('/', async (_req, res) => {
-    res.send('GET /');
-});
+app.use('/uploads', express.static(path.join(__dirname__, 'uploads')));
 
 app.use("/api/users", userRoutes);
 app.use("/api", authRoutes);
@@ -28,6 +26,17 @@ app.use("/api/posts", postRoutes);
 app.use("/api/feed", feedRoutes);
 app.use("/api/users/", adminRoutes)
 app.use("/api/search", searchRoutes)
+
+const clientPath = path.join(__dirname, 'frontend/build')
+app.use(express.static(clientPath))
+
+app.get('*', (req, res) => {
+  if (req.path.startsWith('/api')) {
+    return res.sendStatus(404)
+  }
+
+  res.sendFile(path.join(clientPath, 'index.html'))
+})
 
 const PORT = env.port || 3000;
 
