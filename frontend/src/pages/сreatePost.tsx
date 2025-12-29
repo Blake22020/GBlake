9d14f9eb3b78903a286c9aacaa8883f30a33271f
Blake <mr.blake.hero@mail.ru>
2025-12-29 19:08:15 +0300

    create createPost.tsx without server connection

--- frontend/src/pages/сreatePost.tsx
+++ frontend/src/pages/сreatePost.tsx
@@ -7,7 +7,12 @@ function CreatePost() {
             <LoginNavbarHeader />
             <div className="createPostMain">
                 <div className="createPostCard">
-                    
+                    <form className="createPost">
+                        <h1>Создание поста</h1>
+                        <input placeholder="Заголовок" />
+                        <textarea placeholder="Текст" />
+                        <button>Выложить пост</button>
+                    </form>
                 </div>
             </div>
         </div>
