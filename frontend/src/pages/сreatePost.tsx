95709079d4aac17491cef8315d91b11c4704395f
Blake <mr.blake.hero@mail.ru>
2025-12-29 21:13:34 +0300

    add navigations

--- frontend/src/pages/сreatePost.tsx
+++ frontend/src/pages/сreatePost.tsx
@@ -1,13 +1,18 @@
 import LoginNavbarHeader from "../layouts/loginNavbarHeader";
+import '../styles/pages/createPost.css'
+import { useNavigate } from 'react-router-dom';
+
 
 function CreatePost() {
+    const navigate = useNavigate();
     
     return (
         <div className="createPost">
             <LoginNavbarHeader />
             <div className="createPostMain">
-                <button className="closeButton"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="Layer 2"><g data-name="close"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"/><path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"/></g></g></svg></button>
                 <div className="createPostCard">
+                    <button className="closeButton" onClick={() => {navigate("/")}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><g data-name="Layer 2"><g data-name="close"><rect width="50" height="50" transform="rotate(180 12 12)" opacity="0"/><path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"/></g></g></svg></button>
+
                     <form className="createPostForm">
                         <h1>Создание поста</h1>
                         <input placeholder="Заголовок" />
