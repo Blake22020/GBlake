bd83084d3b9bf7a20fdf78a025a273010910ef06
Blake <mr.blake.hero@mail.ru>
2026-01-12 20:52:58 +0300

    add closeButton

--- frontend/src/pages/Edit.tsx
+++ frontend/src/pages/Edit.tsx
@@ -1,15 +1,16 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import LoginNavbarHeader from "../layouts/loginNavbarHeader";
-import { useNavigate } from "react-router-dom";
-import { getUserData, updateUserProfile, uploadAvatar } from "../services/api";
+import "../styles/pages/edit.css";
+// import { useNavigate } from "react-router-dom";
+// import { getUserData, updateUserProfile, uploadAvatar } from "../services/api";
 
-interface UserData {
-    id: string;
-    username: string;
-    visualName: string;
-    bio: string;
-    avatar: string;
-}
+// interface UserData {
+//     id: string;
+//     username: string;
+//     visualName: string;
+//     bio: string;
+//     avatar: string;
+// }
 
 function Edit() {
     // const navigate = useNavigate();
@@ -99,7 +100,7 @@ function Edit() {
         <div className="editPage">
             <LoginNavbarHeader />
             <div className="editWindow">
-                <button className="closeButton" /* onClick={() => {navigate("/")}} */><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="50" height="50"><g data-name="Layer 2"><g data-name="close"><rect width="50" height="50" transform="rotate(180 12 12)" opacity="0"/><path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"/></g></g></svg></button>
+                <button className="closeButton" /* onClick={() => {navigate("/")}} */><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" ><g data-name="Layer 2"><g data-name="close"><rect width="50" height="50" transform="rotate(180 12 12)" opacity="0"/><path d="M13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29-4.3 4.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l4.29-4.3 4.29 4.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"/></g></g></svg></button>
                 <form className="editForm" /* onSubmit={handleSubmit} */ >
                     <div className="inputs">
                         <div className="addCard">
