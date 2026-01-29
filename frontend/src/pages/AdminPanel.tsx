a94c2529630fbf7fc0016992f47a6ffe8c5e3a07
Blake <mr.blake.hero@mail.ru>
2026-01-29 20:57:26 +0300

    add rol check in admin panel

--- frontend/src/pages/AdminPanel.tsx
+++ frontend/src/pages/AdminPanel.tsx
@@ -1,8 +1,32 @@
+import { useEffect } from "react";
 import LoginNavbarHeader from "../layouts/loginNavbarHeader";
+import { getUser } from "../services/api";
+import { useNavigate } from "react-router-dom";
 
 function AdminPanel() {
+    const navigate = useNavigate();
     
-
+    useEffect(() => {
+        const checkRole = async () => {
+            try {
+                const id = localStorage.getItem('id');
+                if (!id) {
+                    navigate('/404');
+                    return ;
+                }
+                const user = await getUser(id);
+                const role = user.role;
+                if (role < 2) {
+                    navigate('/404');
+                    return ;
+                }
+            } catch(err) {
+                console.error('/404');
+                return ;
+            }
+        }
+        checkRole();
+    }, [navigate])
 
     return (
         <div className="adminPage">
