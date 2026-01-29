72fc842c10afddfc1a3f547230cf7f155c0a45bf
Blake <mr.blake.hero@mail.ru>
2026-01-29 20:41:54 +0300

    start adminPanel page

---
+++ frontend/src/pages/AdminPanel.tsx
@@ -0,0 +1,17 @@
+import LoginNavbarHeader from "../layouts/loginNavbarHeader";
+
+function AdminPanel() {
+    
+
+
+    return (
+        <div className="adminPage">
+            <LoginNavbarHeader />
+            <div className="adminWindow">
+
+            </div>
+        </div>
+    )
+}
+
+export default AdminPanel;
