3464fc50d68e3ecb84f885b0e9b0b720f433042b
blake <mr.blake.hero@mail.ru>
2026-02-01 10:28:19 +0300

    fix errors in api.ts using tsconfig

--- frontend/src/pages/AdminPanel.tsx
+++ frontend/src/pages/AdminPanel.tsx
@@ -5,37 +5,35 @@ import { useNavigate } from "react-router-dom";
 
 function AdminPanel() {
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         const checkRole = async () => {
             try {
-                const id = localStorage.getItem('id');
+                const id = localStorage.getItem("id");
                 if (!id) {
-                    navigate('/404');
-                    return ;
+                    navigate("/404");
+                    return;
                 }
                 const user = await getUser(id);
                 const role = user.role;
                 if (role < 2) {
-                    navigate('/404');
-                    return ;
+                    navigate("/404");
+                    return;
                 }
-            } catch(err) {
-                console.error('/404');
-                return ;
+            } catch (err) {
+                console.error("/404");
+                return;
             }
-        }
+        };
         checkRole();
     }, [navigate]);
 
     return (
         <div className="adminPage">
             <LoginNavbarHeader />
-            <div className="adminWindow">
-                
-            </div>
+            <div className="adminWindow"></div>
         </div>
-    )
+    );
 }
 
 export default AdminPanel;
