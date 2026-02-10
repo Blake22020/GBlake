c666d7cff8416d49e81882287bbc014a1ca74a86
blake <mr.blake.hero@mail.ru>
2026-02-10 21:05:28 +0300

    add modal to admin page

--- frontend/src/pages/AdminPanel.tsx
+++ frontend/src/pages/AdminPanel.tsx
@@ -1,33 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import LoginNavbarHeader from "../layouts/loginNavbarHeader";
 import { getUser } from "../services/api";
 import { useNavigate } from "react-router-dom";
 import "../styles/pages/adminPanel.css";
+import Modal from "../components/Modal";
 
 function AdminPanel() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     const checkRole = async () => {
-    //         try {
-    //             const id = localStorage.getItem("id");
-    //             if (!id) {
-    //                 navigate("/404");
-    //                 return;
-    //             }
-    //             const user = await getUser(id);
-    //             const role = user.role;
-    //             if (role < 2) {
-    //                 navigate("/404");
-    //                 return;
-    //             }
-    //         } catch (err) {
-    //             console.error("/404");
-    //             return;
-    //         }
-    //     };
-    //     checkRole();
-    // }, [navigate]);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalData, setModalData] = useState({ title: "", text: "" });
+    const openModal = (title: string, text: string) => {
+        setModalData({ title, text });
+        setIsModalOpen(true);
+    };
+
+    useEffect(() => {
+        const checkRole = async () => {
+            try {
+                const id = localStorage.getItem("id");
+                if (!id) {
+                    navigate("/404");
+                    return;
+                }
+                const user = await getUser(id);
+                const role = user.role;
+                if (role < 2) {
+                    navigate("/404");
+                    return;
+                }
+            } catch (err) {
+                console.error("/404");
+                return;
+            }
+        };
+        checkRole();
+    }, [navigate]);
 
     return (
         <div className="adminPage">
@@ -44,6 +52,12 @@ function AdminPanel() {
                     </form>
                 </div>
             </div>
+            <Modal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                title={modalData.title}
+                text={modalData.text}
+            />
         </div>
     );
 }
