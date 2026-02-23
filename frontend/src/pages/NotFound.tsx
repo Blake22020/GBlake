bcc44775aec48782e8f1a1c6c1f45fc36d7d7bb8
blake <mr.blake.hero@mail.ru>
2026-02-23 15:29:45 +0300

    finish adaptation for Not Found Page on Tailwind

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import LoginNavbarHeader from "../layouts/loginNavbarHeader";
 import MainNavbarHeader from "../layouts/mainNavbarHeader";
-import "../styles/pages/notFound.css";
 import { setMeta } from "../services/description";
 import { useEffect } from "react";
 
@@ -14,7 +13,7 @@ function NotFound() {
     }, []);
 
     return (
-        <div className="box-border flex pt-[65px] pl-[200px] w-[100dvw] h-[100dvh] object-cover overflow-y-hidden notFoundPage">
+        <div className="box-border flex pt-[65px] max-[900px]:pt-[50px] pl-[200px] max-[600px]:pl-0 max-[900px]:min-[600px]:pl-[200px] w-[100dvw] h-[100dvh] object-cover overflow-y-hidden notFoundPage">
             {localStorage.getItem("token") ? (
                 <LoginNavbarHeader />
             ) : (
@@ -22,15 +21,15 @@ function NotFound() {
             )}
             <div className="flex flex-col flex-1 justify-center items-center gap-[100px] overflow-y-hidden notFoundWindow">
                 <div className="flex flex-col gap-[35px] text-center notFoundText">
-                    <h1 className="drop-shadow-[0_0_200px_rgba(110,91,255,0.45)] font-[700] text-[20rem] text-primary-600 max-[1500px]:min-[500px]:text-[10rem]">
+                    <h1 className="drop-shadow-[0_0_200px_rgba(110,91,255,0.45)] font-[700] text-[20rem] text-primary-600 max-[1500px]:min-[500px]:text-[10rem] max-[350px]:text-[6rem] max-[500px]:min-[350px]:text-[7rem]">
                         404
                     </h1>
-                    <p className="text-[3rem] text-500 text-white max-[1500px]:min-[500px]:text-[2rem]">
+                    <p className="text-[3rem] text-500 text-white max-[1500px]:min-[500px]:text-[2rem] max-[350px]:text-[1.5rem] max-[500px]:min-[350px]:text-[1.9rem]">
                         Такой страницы нет
                     </p>
                 </div>
                 <button
-                    className="bg-primary-600 hover:shadow-[0_0_100px_rgba(110,91,255,0.25)] active:shadow-none px-[30px] py-[20px] border-0 rounded-[55px] text-[2rem] text-500 text-white max-[1500px]:min-[500px]:text-[1.8rem] active:scale-95 transition-all duration-300 ease-in-out cursor-pointer toMainButton"
+                    className="bg-primary-600 hover:shadow-[0_0_100px_rgba(110,91,255,0.25)] active:shadow-none px-[30px] py-[20px] border-0 rounded-[55px] text-[2rem] text-500 text-white max-[1500px]:min-[500px]:text-[1.8rem] max-[350px]:text-[1rem] max-[500px]:min-[350px]:text-[1.3rem] active:scale-95 transition-all duration-300 ease-in-out cursor-pointer toMainButton"
                     onClick={() => {
                         navigate("/");
                     }}
