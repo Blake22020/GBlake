0820538be0b99fb24f2dde4ed46d3cd0e1980bd5
blake <mr.blake.hero@mail.ru>
2026-03-11 21:10:28 +0300

    finish styles for login page on tailwind

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -81,22 +81,22 @@ function Login() {
     return (
         <div className="bg-bg-elevated w-screen h-screen login-main-window">
             <div className="flex justify-center items-center bg-gradient-to-b from-[rgba(110,91,255,0.35)] to-[rgba(11,12,16,1)] w-screen h-screen login-window">
-                <div className="flex flex-col gap-[66px] max-[900px]:gap-[30px] bg-white/10 p-[25px] border-[1.5px] border-white/30 border-solid rounded-[55px] w-[678px] max-[600px]:w-[400px] max-[900px]:w-[500px] text-white login">
-                    <div className="flex justify-between px-[25px] loginPage-header">
-                        <h1 className="flex flex-col after:bg-primary-600 after:mt-[8px] after:rounded-[35px] after:w-full after:h-[6px] font-[700] text-[2rem] text-primary-600 max-[600px]:text-[1.5rem] after:content-[''] cursor-pointer">
+                <div className="flex flex-col gap-[66px] max-[900px]:gap-[30px] bg-white/10 p-[25px] max-[430px]:p-[10px] border-[1.5px] border-white/30 border-solid rounded-[55px] max-[430px]:rounded-[35px] w-[678px] max-[430px]:w-[300px] max-[600px]:w-[400px] max-[900px]:w-[500px] text-white login">
+                    <div className="flex justify-between max-[430px]:justify-between max-[430px]:p-[10px] px-[25px] loginPage-header">
+                        <h1 className="flex flex-col after:bg-primary-600 after:mt-[8px] after:rounded-[35px] after:w-full after:h-[6px] font-[700] text-[2rem] text-primary-600 max-[430px]:text-[1.3rem] max-[600px]:text-[1.5rem] after:content-[''] cursor-pointer">
                             Вход
                         </h1>
                         <h1
                             onClick={() => {
                                 navigate("/register");
                             }}
-                            className="font-[700] text-[2rem] max-[600px]:text-[1.5rem] cursor-pointer"
+                            className="font-[700] text-[2rem] max-[430px]:text-[1.3rem] max-[600px]:text-[1.5rem] cursor-pointer"
                         >
                             Регистрация
                         </h1>
                     </div>
                     <form
-                        className="flex flex-col gap-[40px] max-[600px]:gap-[20px] max-[900px]:gap-[30px] login-form"
+                        className="flex flex-col gap-[40px] max-[430px]:gap-[15px] max-[600px]:gap-[20px] max-[900px]:gap-[30px] login-form"
                         onSubmit={handleSubmit}
                     >
                         <input
