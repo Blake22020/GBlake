5fedb0b78064187263e93d33129250cccae79590
blake <mr.blake.hero@mail.ru>
2026-03-15 15:09:37 +0300

    add styles for likes page on tailwind

--- frontend/src/pages/Likes.tsx
+++ frontend/src/pages/Likes.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { likesPosts } from "../services/api";
 import Modal from "../components/Modal";
 import Post from "../components/Post";
-import '../styles/pages/likes.css';
+import "../styles/pages/likes.css";
 import { setMeta } from "../services/description";
 
 interface PostInterface {
@@ -28,47 +28,49 @@ function Likes() {
         setMeta("description", "Лайки");
     }, []);
 
-
     const [isModalOpen, setIsModalOpen] = useState(false);
-	const [modalData, setModalData] = useState({ title: '', text: '' });
-	const openModal = (title: string, text: string) => {
-		setModalData({ title, text });
-		setIsModalOpen(true);
-	};
+    const [modalData, setModalData] = useState({ title: "", text: "" });
+    const openModal = (title: string, text: string) => {
+        setModalData({ title, text });
+        setIsModalOpen(true);
+    };
 
-    const [posts, setPosts] = useState<any[]>([])
+    const [posts, setPosts] = useState<any[]>([]);
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
+        const token = localStorage.getItem("token");
         if (!token) {
-            navigate('/login');
+            navigate("/login");
         }
 
         const request = async () => {
             try {
                 const res = await likesPosts(token);
-                setPosts(res)
-            } catch(error : any) {
-				const errMsg = error?.response?.data?.message || error.message || 'Неизвестная ошибка';
-				openModal('Ошибка', errMsg);
-			}
-        }
-    }, [])
+                setPosts(res);
+            } catch (error: any) {
+                const errMsg =
+                    error?.response?.data?.message ||
+                    error.message ||
+                    "Неизвестная ошибка";
+                openModal("Ошибка", errMsg);
+            }
+        };
+    }, []);
 
     return (
-        <div className="LikesPage">
+        <div className="flex flex-col pt-[65px] max-[900px]:pt-[50px] pb-[110px] pl-[200px] max-[600px]:pl-0 max-[900px]:pl-[200px] w-screen object-cover LikesPage">
             <LoginNavbarHeader />
-            <div className="LikesWindow">
+            <div className="flex flex-col pt-[150px] pb-[100px] LikesWindow ц-агдд">
                 {posts.map((post: PostInterface) => (
-                        <Post
-                            _id={post._id}
-                            title={post.title}
-                            text={post.text}
-                            createdAt={post.createdAt}
-                            likes={post.likes}
-                            liked={false}
-                            author={post.author}
-                        />
+                    <Post
+                        _id={post._id}
+                        title={post.title}
+                        text={post.text}
+                        createdAt={post.createdAt}
+                        likes={post.likes}
+                        liked={false}
+                        author={post.author}
+                    />
                 ))}
             </div>
             <Modal
@@ -78,7 +80,7 @@ function Likes() {
                 text={modalData.text}
             />
         </div>
-    )
+    );
 }
 
 export default Likes;
